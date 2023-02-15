de7e24837982f4734a9bf988cbc4cb4a0ed2a74f
Noizeisdead <noizee829@gmail.com>
2023-02-15 20:04:07 +0100

    Changed file name

---
+++ Assignment1/src/main/java/org/example/ArrayListExtra.java
@@ -0,0 +1,16 @@
+package org.example;
+import java.util.ArrayList; // import the ArrayList class
+public class ArrayListExtra {
+
+    public void insert(){
+        ArrayList<String> StringArrayList = new ArrayList<String>();
+    }
+
+    public void remove(){
+
+    }
+
+    public void reverse(){
+
+    }
+}
